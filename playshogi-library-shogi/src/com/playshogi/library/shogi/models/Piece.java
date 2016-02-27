62b348a651cc7afa0919157f8eff42894ca41c0c
Jean Fortin <jean.fortin@gmail.com>
2016-02-27 23:36:17 +0100

    PieceType

--- playshogi-library-shogi/src/com/playshogi/library/shogi/models/Piece.java
+++ playshogi-library-shogi/src/com/playshogi/library/shogi/models/Piece.java
@@ -1,5 +1,35 @@
 package com.playshogi.library.shogi.models;
 
-public interface Piece {
+public class Piece {
+
+	private boolean sentePiece;
+	private final PieceType pieceType;
+	private boolean promoted;
+
+	public Piece(final boolean sentePiece, final PieceType pieceType, final boolean promoted) {
+		this.sentePiece = sentePiece;
+		this.pieceType = pieceType;
+		this.promoted = promoted;
+	}
+
+	public boolean isSentePiece() {
+		return sentePiece;
+	}
+
+	public void setSentePiece(final boolean sentePiece) {
+		this.sentePiece = sentePiece;
+	}
+
+	public boolean isPromoted() {
+		return promoted;
+	}
+
+	public void setPromoted(final boolean promoted) {
+		this.promoted = promoted;
+	}
+
+	public PieceType getPieceType() {
+		return pieceType;
+	}
 
 }
--- playshogi-library-shogi/src/com/playshogi/library/shogi/models/position/ShogiBoardState.java
+++ playshogi-library-shogi/src/com/playshogi/library/shogi/models/position/ShogiBoardState.java
@@ -1,5 +1,9 @@
 package com.playshogi.library.shogi.models.position;
 
+import com.playshogi.library.shogi.models.Piece;
+
 public class ShogiBoardState {
+
+	private Piece[][] board;
 
 }
