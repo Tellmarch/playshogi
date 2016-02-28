6bc1d3c2b06ae2eaf9633d45f858bb1096dcabd3
Jean Fortin <jean.fortin@gmail.com>
2016-02-28 09:27:52 +0100

    playshogi website project

---
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package com.playshogi.website.gwt.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
