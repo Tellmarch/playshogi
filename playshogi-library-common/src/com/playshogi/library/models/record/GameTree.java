65b3cb848b2f8d8c53a6cc183ffdb501b61b6b41
Jean Fortin <jean.fortin@gmail.com>
2016-03-12 19:24:28 +0100

    Game tree

--- playshogi-library-common/src/com/playshogi/library/models/record/GameTree.java
+++ playshogi-library-common/src/com/playshogi/library/models/record/GameTree.java
@@ -2,4 +2,14 @@ package com.playshogi.library.models.record;
 
 public class GameTree {
 
+	private final Node rootNode;
+
+	public GameTree(final Node rootNode) {
+		this.rootNode = rootNode;
+	}
+
+	public Node getRootNode() {
+		return rootNode;
+	}
+
 }
---
+++ playshogi-library-common/src/com/playshogi/library/models/record/Node.java
@@ -0,0 +1,48 @@
+package com.playshogi.library.models.record;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.playshogi.library.models.Move;
+
+public class Node {
+
+	private Node parent = null;
+	private final List<Node> children = new ArrayList<>();
+
+	private Move move;
+	private String comment = null;
+
+	public Node(final Move move) {
+		this.move = move;
+	}
+
+	public Node getParent() {
+		return parent;
+	}
+
+	public void setParent(final Node parent) {
+		this.parent = parent;
+	}
+
+	public Move getMove() {
+		return move;
+	}
+
+	public void setMove(final Move move) {
+		this.move = move;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(final String comment) {
+		this.comment = comment;
+	}
+
+	public List<Node> getChildren() {
+		return children;
+	}
+
+}
