7cc061cab38cbaab49da0d163c614d60d8c8fa89
Jean Fortin <jean.fortin@gmail.com>
2016-03-23 18:47:11 +0100

    MovePlayed Event

--- playshogi-website-gwt/src/com/playshogi/website/gwt/client/events/MovePlayedEvent.java
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/client/events/MovePlayedEvent.java
@@ -3,5 +3,14 @@ package com.playshogi.website.gwt.client.events;
 import com.playshogi.library.shogi.models.moves.ShogiMove;
 
 public class MovePlayedEvent {
-	private ShogiMove move;
+	private final ShogiMove move;
+
+	public MovePlayedEvent(final ShogiMove move) {
+		this.move = move;
+	}
+
+	public ShogiMove getMove() {
+		return move;
+	}
+
 }
