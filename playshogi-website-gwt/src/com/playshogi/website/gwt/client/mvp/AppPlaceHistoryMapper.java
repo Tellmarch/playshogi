055fd71b08cce04206bd7b73a8532e9793ea6e7c
Jean Fortin <jean.fortin@gmail.com>
2016-03-30 19:44:31 +0200

    Board tokenizer

--- playshogi-website-gwt/src/com/playshogi/website/gwt/client/mvp/AppPlaceHistoryMapper.java
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/client/mvp/AppPlaceHistoryMapper.java
@@ -2,9 +2,10 @@ package com.playshogi.website.gwt.client.mvp;
 
 import com.google.gwt.place.shared.PlaceHistoryMapper;
 import com.google.gwt.place.shared.WithTokenizers;
+import com.playshogi.website.gwt.client.place.FreeBoardPlace;
 import com.playshogi.website.gwt.client.place.MainPagePlace;
 import com.playshogi.website.gwt.client.place.TsumePlace;
 
-@WithTokenizers({ MainPagePlace.Tokenizer.class, TsumePlace.Tokenizer.class })
+@WithTokenizers({ MainPagePlace.Tokenizer.class, TsumePlace.Tokenizer.class, FreeBoardPlace.Tokenizer.class })
 public interface AppPlaceHistoryMapper extends PlaceHistoryMapper {
 }
