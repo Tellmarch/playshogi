e7e9bc38083226892770a3fdf3d3954a9d843fc4
Jean Fortin <jean.fortin@gmail.com>
2016-04-14 09:00:06 +0200

    authentication mock

--- playshogi-library-database/src/com/playshogi/library/database/Users.java
+++ playshogi-library-database/src/com/playshogi/library/database/Users.java
@@ -2,4 +2,13 @@ package com.playshogi.library.database;
 
 public class Users {
 
+	private final DbConnection dbConnection;
+
+	public Users(final DbConnection dbConnection) {
+		this.dbConnection = dbConnection;
+	}
+
+	public boolean authenticateUser(final String username, final String password) {
+		return true;
+	}
 }
