40be3e2c7ac6e5f5eaf51762b5e9da9c11f0599b
Jean Fortin <jean.fortin@gmail.com>
2016-04-21 00:02:31 +0200

    test

--- playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/LoginView.java
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/LoginView.java
@@ -1,26 +1,11 @@
 package com.playshogi.website.gwt.client.ui;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.ButtonElement;
-import com.google.gwt.dom.client.InputElement;
 import com.google.gwt.dom.client.Node;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
 
 public class LoginView extends Composite implements ClickHandler {
-	interface Binder extends UiBinder<Widget, LoginView> {
-	}
-
-	private final Binder uiBinder = GWT.create(Binder.class);
-
-	@UiField InputElement email;
-	@UiField InputElement password;
-	@UiField ButtonElement login;
-	@UiField ButtonElement register;
 
 	public LoginView() {
 
