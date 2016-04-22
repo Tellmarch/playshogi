f2447d815f27fae11346c1985b42eaf04dc636a9
Jean Fortin <jean.fortin@gmail.com>
2016-04-22 20:24:10 +0200

    cleanup

--- playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/LoginView.java
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/LoginView.java
@@ -1,7 +1,5 @@
 package com.playshogi.website.gwt.client.ui;
 
-import com.google.gwt.dom.client.Node;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Composite;
 
@@ -9,19 +7,6 @@ public class LoginView extends Composite implements ClickHandler {
 
 	public LoginView() {
 
-		initWidget(uiBinder.createAndBindUi(this));
-
-		this.addDomHandler(this, ClickEvent.getType());
-	}
-
-	@Override
-	public void onClick(final ClickEvent event) {
-		Node target = event.getNativeEvent().getEventTarget().<Node> cast();
-		if (login.isOrHasChild(target)) {
-			// presenter.login(email.getValue(), password.getValue());
-		} else if (register.isOrHasChild(target)) {
-			// presenter.register(email.getValue(), password.getValue());
-		}
 	}
 
 }
