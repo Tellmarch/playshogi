4cbb4b52818b83705c22c716556ebe333188cc9a
Jean Fortin <jean.fortin@gmail.com>
2016-04-22 20:24:26 +0200

    cleanup

--- playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/LoginView.java
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/LoginView.java
@@ -1,9 +1,8 @@
 package com.playshogi.website.gwt.client.ui;
 
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Composite;
 
-public class LoginView extends Composite implements ClickHandler {
+public class LoginView extends Composite {
 
 	public LoginView() {
 
