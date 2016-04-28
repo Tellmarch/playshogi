26f1d8cf0cb4a37766a51dda09209a089384715a
Jean Fortin <jean.fortin@gmail.com>
2016-04-28 21:16:12 +0200

    users

--- playshogi-website-gwt/src/com/playshogi/website/gwt/server/LoginServiceImpl.java
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/server/LoginServiceImpl.java
@@ -1,11 +1,14 @@
 package com.playshogi.website.gwt.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.playshogi.library.database.Users;
 import com.playshogi.website.gwt.client.services.LoginService;
 
 public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
 
 	private static final long serialVersionUID = 1L;
+
+	private Users users;
 
 	@Override
 	public String login(final String username, final String password) {
