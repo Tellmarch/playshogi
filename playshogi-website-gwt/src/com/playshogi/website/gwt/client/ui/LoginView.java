bf0f720ba9cd1141090a2d9f6af35f1c64bc5342
Jean Fortin <jean.fortin@gmail.com>
2016-05-05 21:47:22 +0200

    username and password fields

--- playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/LoginView.java
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/LoginView.java
@@ -3,6 +3,7 @@ package com.playshogi.website.gwt.client.ui;
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
@@ -14,19 +15,24 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.playshogi.website.gwt.client.services.LoginService;
 import com.playshogi.website.gwt.client.services.LoginServiceAsync;
 
-public class LoginView extends Composite implements ClickHandler {
+public class LoginView extends Composite implements ClickHandler, AsyncCallback<Boolean> {
 
 	private final Button loginButton;
 
 	private final LoginServiceAsync loginService = GWT.create(LoginService.class);
 
+	private final TextBox usernameTextBox;
+	private final PasswordTextBox passwordTextBox;
+
 	public LoginView() {
 
 		Grid grid = new Grid(2, 2);
 		grid.setWidget(0, 0, new HTML("Username:"));
-		grid.setWidget(0, 1, new TextBox());
+		usernameTextBox = new TextBox();
+		grid.setWidget(0, 1, usernameTextBox);
 		grid.setWidget(1, 0, new HTML("Password:"));
-		grid.setWidget(1, 1, new PasswordTextBox());
+		passwordTextBox = new PasswordTextBox();
+		grid.setWidget(1, 1, passwordTextBox);
 
 		HorizontalPanel buttonPanel = new HorizontalPanel();
 
@@ -46,8 +52,20 @@ public class LoginView extends Composite implements ClickHandler {
 	public void onClick(final ClickEvent event) {
 		Object source = event.getSource();
 		if (source == loginButton) {
-			loginService.login(username, password, this);
+			loginService.login(usernameTextBox.getText(), passwordTextBox.getText(), this);
 		}
+	}
+
+	@Override
+	public void onSuccess(final Boolean result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onFailure(final Throwable caught) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
