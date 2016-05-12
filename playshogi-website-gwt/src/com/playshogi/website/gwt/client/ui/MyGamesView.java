081371c9f4caf0e02c743696d80787e2d97b7099
Jean Fortin <jean.fortin@gmail.com>
2016-05-12 23:11:34 +0200

    Singleton view

--- playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/MyGamesView.java
+++ playshogi-website-gwt/src/com/playshogi/website/gwt/client/ui/MyGamesView.java
@@ -1,7 +1,9 @@
 package com.playshogi.website.gwt.client.ui;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.inject.Singleton;
 
+@Singleton
 public class MyGamesView extends Composite {
 
 }
