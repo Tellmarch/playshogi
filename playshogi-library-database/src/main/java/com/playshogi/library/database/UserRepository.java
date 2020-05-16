2ff0d97469c4ffbbddee3aa844ad2302de0df1ba
Tellmarch <jean.fortin@gmail.com>
2020-05-16 16:26:37 +0200

    Insert user in DB

--- playshogi-library-database/src/main/java/com/playshogi/library/database/UserRepository.java
+++ playshogi-library-database/src/main/java/com/playshogi/library/database/UserRepository.java
@@ -1,9 +1,6 @@
 package com.playshogi.library.database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +9,9 @@ public class UserRepository {
     private static final Logger LOGGER = Logger.getLogger(UserRepository.class.getName());
 
     private static final String LOGIN_SQL = "SELECT * FROM ps_user WHERE username = ? AND password_hash = ? ";
+    private static final String INSERT_USER = "INSERT INTO `playshogi`.`ps_user` "
+            + "(`username`, `password_hash`)" + " VALUES ( ?, ?);";
+
     private final DbConnection dbConnection;
 
     public UserRepository(final DbConnection dbConnection) {
@@ -37,7 +37,35 @@ public class UserRepository {
         }
     }
 
+    public int insertUser(final String userName, final String password_hash) {
+
+        int key = -1;
+
+        Connection connection = dbConnection.getConnection();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER,
+                Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, userName);
+            preparedStatement.setString(2, password_hash);
+            preparedStatement.executeUpdate();
+
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+
+            if (rs.next()) {
+                key = rs.getInt(1);
+                LOGGER.log(Level.INFO, "Inserted user with index " + key);
+            } else {
+                LOGGER.log(Level.SEVERE, "Could not insert user");
+            }
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Error saving the user in db", e);
+        }
+
+        return key;
+    }
+
+
     public static void main(final String[] args) {
+        new UserRepository(new DbConnection()).insertUser("Tellmarch", "test");
         new UserRepository(new DbConnection()).authenticateUser("Tellmarch", "test");
         new UserRepository(new DbConnection()).authenticateUser("Tellmarch", "test2");
     }
