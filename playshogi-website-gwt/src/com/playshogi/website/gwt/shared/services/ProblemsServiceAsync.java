66d7e358cee8e063f752ebe01d86097669cfddd5
Tellmarch <jean.fortin@gmail.com>
2020-05-21 09:57:12 +0200

    Delete old project

--- playshogi-website-gwt/src/com/playshogi/website/gwt/shared/services/ProblemsServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.playshogi.website.gwt.shared.services;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ProblemsServiceAsync {
-
-	void getProblemUsf(String problemId, AsyncCallback<String> callback);
-
-}
