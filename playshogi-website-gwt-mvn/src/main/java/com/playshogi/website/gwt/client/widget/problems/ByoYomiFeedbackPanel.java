1e322eded5b87470a970d1771f2ac52be602f9e0
Daniel Dugovic <dandydand@gmail.com>
2020-08-31 03:27:48 -0500

    Hide SFEN during Byo-Yomi Survival

--- playshogi-website-gwt-mvn/src/main/java/com/playshogi/website/gwt/client/widget/problems/ByoYomiFeedbackPanel.java
+++ playshogi-website-gwt-mvn/src/main/java/com/playshogi/website/gwt/client/widget/problems/ByoYomiFeedbackPanel.java
@@ -9,9 +9,7 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.event.shared.binder.EventBinder;
 import com.google.web.bindery.event.shared.binder.EventHandler;
-import com.playshogi.library.shogi.models.formats.sfen.SfenConverter;
 import com.playshogi.website.gwt.client.events.ByoYomiSurvivalFinishedEvent;
-import com.playshogi.website.gwt.client.events.PositionChangedEvent;
 import com.playshogi.website.gwt.client.events.UserFinishedProblemEvent;
 import com.playshogi.website.gwt.client.events.UserNavigatedBackEvent;
 import com.playshogi.website.gwt.client.widget.timer.ByoYomiTimerPanel;
@@ -32,7 +30,6 @@ public class ByoYomiFeedbackPanel extends Composite {
     private EventBus eventBus;
 
     private final HTML messagePanel;
-    private final HTML positionPanel;
     private final HTML summaryPanel;
 
     private final ByoYomiTimerPanel byoTomiTimerPanel;
@@ -51,11 +48,6 @@ public class ByoYomiFeedbackPanel extends Composite {
         messagePanel.getElement().getStyle().setBackgroundColor("White");
 
         flowPanel.add(messagePanel);
-
-        positionPanel = new HTML();
-        positionPanel.getElement().getStyle().setBackgroundColor("White");
-
-        flowPanel.add(positionPanel);
 
         summaryPanel = new HTML();
         summaryPanel.getElement().getStyle().setBackgroundColor("White");
@@ -84,9 +76,10 @@ public class ByoYomiFeedbackPanel extends Composite {
     @EventHandler
     public void onByoYomiSurvivalFinishedEvent(final ByoYomiSurvivalFinishedEvent event) {
         GWT.log("Problem feedback: handle ByoYomiSurvivalFinishedEvent");
-        summaryPanel.setHTML(SafeHtmlUtils.fromTrustedString("<p style=\"font-size:20px;" +
-                "color:black\">Event complete! </br> Final Score: " + event.getFinalScore() +
-                " </br> Total time: " + event.getTotalTimeSec() + "s</p>"));
+        summaryPanel.setHTML(SafeHtmlUtils.fromTrustedString("<p style=\"font-size:20px;color:black\">" +
+                "Event complete!<br/>" +
+                "Final score: " + event.getFinalScore() + " <br/>" +
+                "Total time: " + event.getTotalTimeSec() + "s</p>"));
         summaryPanel.setVisible(true);
     }
 
@@ -95,20 +88,11 @@ public class ByoYomiFeedbackPanel extends Composite {
         this.eventBus = eventBus;
         eventBinder.bindEventHandlers(this, eventBus);
         messagePanel.setHTML(chooseHtml);
-        positionPanel.setVisible(false);
         summaryPanel.setVisible(false);
         byoTomiTimerPanel.activate(eventBus);
     }
 
     public void setTimerVisible(boolean visible) {
         byoTomiTimerPanel.setVisible(visible);
-    }
-
-    @EventHandler
-    public void onPositionChanged(final PositionChangedEvent event) {
-        GWT.log("Problem feedback: position changed");
-        SafeHtml html = SafeHtmlUtils.fromTrustedString("<nobr>" + SfenConverter.toSFENWithMoveCount(event.getPosition()) + "</nobr>");
-        positionPanel.setHTML(html);
-        positionPanel.setVisible(true);
     }
 }
