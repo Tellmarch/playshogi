1cd7c6d4f79a8610ddfd9ccdb909e38d91dd6342
Tellmarch <jean.fortin@gmail.com>
2020-09-13 09:37:07 +0200

    Script to build packaged war

---
+++ buildprod.sh
@@ -0,0 +1,6 @@
+mvn -B install --file playshogi-library-common/pom.xml
+mvn -B install --file playshogi-library-shogi/pom.xml
+mvn -B install --file playshogi-library-shogi-files/pom.xml
+mvn -B install --file playshogi-library-database/pom.xml
+mvn -B install --file playshogi-library-shogi-engine/pom.xml
+mvn -B package --file playshogi-website-gwt-mvn/pom.xml
