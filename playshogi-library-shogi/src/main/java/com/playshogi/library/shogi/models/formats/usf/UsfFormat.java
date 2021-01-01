631c6916945375c984ab8c3fb7409e5bd79f7cff
Tellmarch <jean.fortin@gmail.com>
2021-01-01 10:45:46 +0100

    Refactor USF parsing

--- playshogi-library-shogi/src/main/java/com/playshogi/library/shogi/models/formats/usf/UsfFormat.java
+++ playshogi-library-shogi/src/main/java/com/playshogi/library/shogi/models/formats/usf/UsfFormat.java
@@ -5,20 +5,14 @@ import com.playshogi.library.shogi.models.formats.sfen.LineReader;
 import com.playshogi.library.shogi.models.formats.sfen.SfenConverter;
 import com.playshogi.library.shogi.models.formats.sfen.StringLineReader;
 import com.playshogi.library.shogi.models.moves.EditMove;
-import com.playshogi.library.shogi.models.moves.Move;
 import com.playshogi.library.shogi.models.moves.ShogiMove;
 import com.playshogi.library.shogi.models.record.*;
-import com.playshogi.library.shogi.rules.ShogiRulesEngine;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public enum UsfFormat implements GameRecordFormat {
     INSTANCE;
-
-    private static final Logger LOGGER = Logger.getLogger(UsfFormat.class.getName());
 
     public GameRecord readSingle(final String string) {
         List<GameRecord> gameRecords = read(string);
@@ -36,192 +30,29 @@ public enum UsfFormat implements GameRecordFormat {
     @Override
     public List<GameRecord> read(final LineReader lineReader) {
         String l = lineReader.nextLine();
-        // First, check that the file is indeed USF.
         if (!l.contains("USF:")) {
             throw (new IllegalArgumentException("Not a recognized USF File. Maybe wrong encoding?"));
         }
 
-        // Go to the start of the next game
-        while (!l.startsWith("^")) {
-            l = lineReader.nextLine();
+        ArrayList<GameRecord> games = new ArrayList<>(1);
+
+        goToNextGame(lineReader);
+        while (isAtNextGame(lineReader)) {
+            games.add(new UsfGameParser(lineReader).readGameRecord());
         }
-
-        // This line contains the "preview" line
-
-        // Reads the result
-        char resultc = l.charAt(1);
-
-        GameResult gameResult = getResult(resultc);
-
-        // We will know start building the game tree
-
-        GameTree gameTree;
-        // If the next character is ":", the game is starting from start
-        // position.
-        if (l.charAt(2) == ':') {
-            gameTree = new GameTree();
-        } else {
-            // We read the starting position, in a SFEN that goes up to ":"
-            String sfen = l.substring(2, l.indexOf(':'));
-            if (SfenConverter.INITIAL_POSITION_SFEN.equals(sfen)) {
-                gameTree = new GameTree();
-            } else {
-                gameTree = new GameTree(SfenConverter.fromSFEN(sfen));
-            }
-        }
-
-        GameNavigation gameNavigation = new GameNavigation(new ShogiRulesEngine(), gameTree);
-
-        // What follows is the move sequence
-        String moves = l.substring(l.indexOf(':') + 1);
-
-        playMoveSequence(gameNavigation, moves);
-
-        GameInformation gameInformation = new GameInformation();
-
-        // Now, we read tag lines. We read every lines until the end of the
-        // file,
-        // or the start of another game, identified by "^",.
-        while (lineReader.hasNextLine()) {
-            l = lineReader.nextLine();
-
-            // If the line is empty, skip it
-            if (l.isEmpty()) {
-                continue;
-            }
-
-            if (l.charAt(0) == '^') {
-                // New line
-                break;
-            }
-
-            // A line starting with . changes the current node
-            if (l.charAt(0) == '.') {
-                String r = l.substring(1);
-                // Next can be 1)Nothing 2)Move number 3)New move
-                // 4)MoveNumber:new move
-                if (r.isEmpty()) {
-                    // Empty : we just go to the next move
-                    gameNavigation.moveForwardInLastVariation();
-                } else {
-                    String[] rs = r.split(":");
-                    if (rs.length == 1) {
-                        // No :, case 2 or 3.
-                        // We try to read a move number
-                        try {
-                            int mn = Integer.parseInt(r);
-                            // We found a move number, we go there
-                            gameNavigation.goToNodeUSF(mn);
-                        } catch (NumberFormatException e) {
-                            // If it wasn't a number, it should be a move
-                            // sequence
-                            playMoveSequence(gameNavigation, r);
-                        }
-                    } else if (rs.length == 2) {
-                        // it should be a move number + a move sequence
-                        int mn = Integer.parseInt(rs[0]);
-                        // We found a move number, we go there
-                        gameNavigation.goToNodeUSF(mn);
-                        // next should be a move sequence
-                        playMoveSequence(gameNavigation, rs[1]);
-                    } else {
-                        // More that one ":" in one line? shouldn't happen
-                        throw (new IllegalArgumentException("Error parsing the USF File (in the line " + l + " )"));
-                    }
-
-                }
-                continue;
-            }
-
-            // A line starting with # is a comment
-            if (l.charAt(0) == '#') {
-                // we just add the comment line to the current node's
-                // comment
-                gameNavigation.getCurrentNode().setComment(gameNavigation.getCurrentNode().getComment() + "\n" + l.substring(1));
-                continue;
-            }
-
-            // Next, we search for header tags or node tags
-            if (l.startsWith("SFEN:")) {
-                // Changes the position
-                String sfen = l.substring(5);
-                // gameTree.addEdit(sfen);
-                continue;
-            }
-
-            if (l.startsWith("BN:")) {
-                String blackName = l.substring(3);
-                gameInformation.setSente(blackName);
-                continue;
-            } else if (l.startsWith("WN:")) {
-                String whiteName = l.substring(3);
-                gameInformation.setGote(whiteName);
-                continue;
-            } else if (l.startsWith("GD:")) {
-                String gameDate = l.substring(3);
-                gameInformation.setDate(gameDate);
-                continue;
-            } else if (l.startsWith("GQ:")) {
-                String gameVenue = l.substring(3);
-                gameInformation.setVenue(gameVenue);
-                continue;
-            }
-
-            // A line starting with ~ is an object
-            if (l.charAt(0) == '~') {
-                // USFObject object = new USFObject(l.substring(1));
-                // gameTree.getCurrent().addObject(object);
-                continue;
-            }
-
-            // A line starting with "X" is a custom tag
-            if (l.charAt(0) == 'X') {
-                // String tag = l.substring(1);
-                // gameTree.getCurrent().addTag(l);
-                continue;
-            }
-        }
-
-        // TODO
-        // switch (gameTree.getCurrent().getMove().getSpecialType()) {
-        // case Move.SPECIAL_BREK:
-        // result = Game.RESULT_BREAK;
-        // break;
-        // case Move.SPECIAL_JISO:
-        // result = Game.RESULT_JISHOGI;
-        // break;
-        // case Move.SPECIAL_REPT:
-        // result = Game.RESULT_SENNICHITE;
-        // break;
-        // case Move.SPECIAL_RSGN:
-        // if (bsente) {
-        // result = Game.RESULT_SENTE;
-        // } else {
-        // result = Game.RESULT_GOTE;
-        // }
-        // }
-
-        gameNavigation.moveToStart();
-
-        return Arrays.asList(new GameRecord(gameInformation, gameTree, gameResult));
+        return games;
     }
 
-    /**
-     * plays a move sequence represented by a String, with each move occupying 4
-     * characters.
-     */
-    private static void playMoveSequence(final GameNavigation gameNavigation, final String moves) {
-        // each move takes exactly 4 characters
-        int numberOfMoves = moves.length() / 4;
-        for (int i = 0; i < numberOfMoves; i++) {
-            String move = moves.substring(4 * i, 4 * i + 4);
-            LOGGER.log(Level.FINE, "Parsing move: " + move);
-            Move curMove = UsfMoveConverter.fromUsfString(move, gameNavigation.getPosition());
-            if (curMove == null || !move.equals(curMove.toString())) {
-                LOGGER.log(Level.SEVERE, "Error parsing move: " + move + " resulted in move: " + curMove);
-            }
-            gameNavigation.addMove(curMove);
+    private void goToNextGame(final LineReader lineReader) {
+        while (!isAtNextGame(lineReader)) {
+            lineReader.nextLine();
         }
+    }
+
+    private boolean isAtNextGame(final LineReader lineReader) {
+        return lineReader.hasNextLine() &&
+                !lineReader.peekNextLine().isEmpty() &&
+                lineReader.peekNextLine().charAt(0) == '^';
     }
 
     @Override
@@ -277,29 +108,6 @@ public enum UsfFormat implements GameRecordFormat {
 
         }
         return builder.toString();
-    }
-
-    private GameResult getResult(final char resultc) {
-        GameResult gameResult = GameResult.UNKNOWN;
-
-        switch (resultc) {
-            case 's':
-            case 'b':
-                gameResult = GameResult.BLACK_WIN;
-                break;
-            case 'g':
-            case 'w':
-                gameResult = GameResult.WHITE_WIN;
-                break;
-            case 'd':
-                gameResult = GameResult.OTHER;
-                break;
-            case '*':
-                break;
-            default:
-                throw (new IllegalArgumentException("Error parsing the USF File (not a valid result)"));
-        }
-        return gameResult;
     }
 
     private static char getResultChar(final GameResult gameResult) {
---
+++ playshogi-library-shogi/src/main/java/com/playshogi/library/shogi/models/formats/usf/UsfGameParser.java
@@ -0,0 +1,251 @@
+package com.playshogi.library.shogi.models.formats.usf;
+
+import com.playshogi.library.shogi.models.formats.sfen.LineReader;
+import com.playshogi.library.shogi.models.formats.sfen.SfenConverter;
+import com.playshogi.library.shogi.models.moves.Move;
+import com.playshogi.library.shogi.models.record.*;
+import com.playshogi.library.shogi.rules.ShogiRulesEngine;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Reads one single game from an USF file
+ * Not reusable - create a new instance for each game to parse (mostly because it is not thread-safe)
+ */
+class UsfGameParser {
+
+    private static final Logger LOGGER = Logger.getLogger(UsfGameParser.class.getName());
+
+    private final LineReader lineReader;
+    private GameInformation gameInformation;
+    private GameResult gameResult;
+    private GameTree gameTree;
+    private GameNavigation gameNavigation;
+
+    UsfGameParser(final LineReader lineReader) {
+        this.lineReader = lineReader;
+    }
+
+    // When we call this method, the next line should start with ^ (The game preview tag)
+    GameRecord readGameRecord() {
+        gameInformation = new GameInformation();
+
+        readPreviewLine();
+        readGameTags();
+
+        while (isAtNextNode()) {
+            readNode();
+        }
+
+        gameNavigation.moveToStart();
+
+        return new GameRecord(gameInformation, gameTree, gameResult);
+    }
+
+    private void readGameTags() {
+        while (lineReader.hasNextLine() && !isAtNextGame() && !isAtNextNode()) {
+            readGameTag();
+        }
+    }
+
+    private void readNode() {
+        readDotLine(lineReader.nextLine());
+        while (lineReader.hasNextLine() && !isAtNextGame() && !isAtNextNode()) {
+            readNodeTag();
+        }
+    }
+
+    private void readGameTag() {
+        String line = lineReader.nextLine();
+        if (line.isEmpty()) {
+            return;
+        }
+
+        if (line.startsWith("BN:")) {
+            String blackName = line.substring(3);
+            gameInformation.setSente(blackName);
+        } else if (line.startsWith("WN:")) {
+            String whiteName = line.substring(3);
+            gameInformation.setGote(whiteName);
+        } else if (line.startsWith("GD:")) {
+            String gameDate = line.substring(3);
+            gameInformation.setDate(gameDate);
+        } else if (line.startsWith("GQ:")) {
+            String gameVenue = line.substring(3);
+            gameInformation.setVenue(gameVenue);
+        } else {
+            System.out.println("Ignoring game tag: " + line);
+        }
+    }
+
+
+    private void readNodeTag() {
+        String line = lineReader.nextLine();
+        if (line.isEmpty()) {
+            return;
+        }
+
+        // A line starting with # is a comment
+        if (line.charAt(0) == '#') {
+            readComment(line);
+        }
+
+        if (line.startsWith("SFEN:")) {
+            readSfenTag(line);
+        }
+
+        // A line starting with ~ is an object
+        if (line.charAt(0) == '~') {
+            // USFObject object = new USFObject(l.substring(1));
+            // gameTree.getCurrent().addObject(object);
+        }
+
+        // A line starting with "X" is a custom tag
+        if (line.charAt(0) == 'X') {
+            // String tag = l.substring(1);
+            // gameTree.getCurrent().addTag(l);
+        }
+    }
+
+    private void readSfenTag(final String line) {
+        // Changes the position
+        String sfen = line.substring(5);
+        // gameTree.addEdit(sfen);
+    }
+
+    private void readComment(final String line) {
+        // we just add the comment line to the current node's comment
+        gameNavigation.getCurrentNode().setComment(gameNavigation.getCurrentNode().getComment() + "\n" + line.substring(1));
+    }
+
+    private void readDotLine(final String l) {
+        if (!(l.charAt(0) == '.')) {
+            throw new IllegalArgumentException("New USF node should start with . - actual " + l);
+        }
+
+        String r = l.substring(1);
+        // Next can be 1)Nothing 2)Move number 3)New move
+        // 4)MoveNumber:new move
+        if (r.isEmpty()) {
+            // Empty : we just go to the next move
+            gameNavigation.moveForwardInLastVariation();
+        } else {
+            String[] rs = r.split(":");
+            if (rs.length == 1) {
+                // No :, case 2 or 3.
+                // We try to read a move number
+                try {
+                    int mn = Integer.parseInt(r);
+                    // We found a move number, we go there
+                    gameNavigation.goToNodeUSF(mn);
+                } catch (NumberFormatException e) {
+                    // If it wasn't a number, it should be a move
+                    // sequence
+                    playMoveSequence(gameNavigation, r);
+                }
+            } else if (rs.length == 2) {
+                // it should be a move number + a move sequence
+                int mn = Integer.parseInt(rs[0]);
+                // We found a move number, we go there
+                gameNavigation.goToNodeUSF(mn);
+                // next should be a move sequence
+                playMoveSequence(gameNavigation, rs[1]);
+            } else {
+                // More that one ":" in one line? shouldn't happen
+                throw (new IllegalArgumentException("Error parsing the USF File (in the line " + l + " )"));
+            }
+
+        }
+    }
+
+    private void readPreviewLine() {
+        String preview = lineReader.nextLine();
+
+        if (!(preview.charAt(0) == '^')) {
+            throw new IllegalArgumentException("New USF game should start with ^ - actual " + preview);
+        }
+
+        //The preview String contains the game result, initial position, and main line moves.
+        gameResult = getResult(preview.charAt(1));
+        gameTree = createEmptyGameTreeWithStartingPosition(preview);
+
+        gameNavigation = new GameNavigation(new ShogiRulesEngine(), gameTree);
+
+        // What follows is the move sequence
+        String previewMoves = preview.substring(preview.indexOf(':') + 1);
+        playMoveSequence(gameNavigation, previewMoves);
+
+        //TODO refine result based on last move
+    }
+
+    private GameTree createEmptyGameTreeWithStartingPosition(final String preview) {
+        GameTree gameTree;
+        // If the next character is ":", the game is starting from the default starting position.
+
+        if (preview.charAt(2) == ':') {
+            gameTree = new GameTree();
+        } else {
+            // We read the starting position, in a SFEN that goes up to ":"
+            String sfen = preview.substring(2, preview.indexOf(':'));
+            if (SfenConverter.INITIAL_POSITION_SFEN.equals(sfen)) {
+                gameTree = new GameTree();
+            } else {
+                gameTree = new GameTree(SfenConverter.fromSFEN(sfen));
+            }
+        }
+        return gameTree;
+    }
+
+    /**
+     * plays a move sequence represented by a String, with each move occupying 4 characters.
+     */
+    private static void playMoveSequence(final GameNavigation gameNavigation, final String moves) {
+        // each move takes exactly 4 characters
+        int numberOfMoves = moves.length() / 4;
+        for (int i = 0; i < numberOfMoves; i++) {
+            String move = moves.substring(4 * i, 4 * i + 4);
+            Move curMove = UsfMoveConverter.fromUsfString(move, gameNavigation.getPosition());
+            if (curMove == null || !move.equals(curMove.toString())) {
+                LOGGER.log(Level.SEVERE, "Error parsing move: " + move + " resulted in move: " + curMove);
+            }
+            gameNavigation.addMove(curMove);
+        }
+    }
+
+    private GameResult getResult(final char resultc) {
+        GameResult gameResult = GameResult.UNKNOWN;
+
+        switch (resultc) {
+            case 's':
+            case 'b':
+                gameResult = GameResult.BLACK_WIN;
+                break;
+            case 'g':
+            case 'w':
+                gameResult = GameResult.WHITE_WIN;
+                break;
+            case 'd':
+                gameResult = GameResult.OTHER;
+                break;
+            case '*':
+                break;
+            default:
+                throw (new IllegalArgumentException("Error parsing the USF File (not a valid result)"));
+        }
+        return gameResult;
+    }
+
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    private boolean isAtNextGame() {
+        return lineReader.hasNextLine() &&
+                !lineReader.peekNextLine().isEmpty() &&
+                lineReader.peekNextLine().charAt(0) == '^';
+    }
+
+    private boolean isAtNextNode() {
+        return lineReader.hasNextLine() &&
+                !lineReader.peekNextLine().isEmpty() &&
+                lineReader.peekNextLine().charAt(0) == '.';
+    }
+}
