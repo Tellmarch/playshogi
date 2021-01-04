44ee43847e4cfc57e4893d1d513638acf57acc6b
Tellmarch <jean.fortin@gmail.com>
2021-01-04 17:01:14 +0100

    Update Version

--- playshogi-website-gwt-mvn/src/main/java/com/playshogi/website/gwt/shared/Version.java
+++ playshogi-website-gwt-mvn/src/main/java/com/playshogi/website/gwt/shared/Version.java
@@ -1,5 +1,5 @@
 package com.playshogi.website.gwt.shared;
 
 public class Version {
-    public static final String VERSION = "2020-12-05.1";
+    public static final String VERSION = "2021-01-04.1";
 }
