79b6647cf05706d8187b7843a932666334af6b26
SleepingChinchilla <oneye@windowslive.com>
2021-04-24 18:01:59 +0900

    Added 'what is tsumeshogi' button

--- playshogi-website/playshogi-website-client/src/main/java/com/playshogi/website/gwt/client/ui/TsumeView.java
+++ playshogi-website/playshogi-website-client/src/main/java/com/playshogi/website/gwt/client/ui/TsumeView.java
@@ -1,7 +1,12 @@
 package com.playshogi.website.gwt.client.ui;
 
 import com.google.gwt.core.shared.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -11,6 +16,16 @@ import com.playshogi.website.gwt.client.widget.board.ShogiBoard;
 import com.playshogi.website.gwt.client.widget.gamenavigator.GameNavigator;
 import com.playshogi.website.gwt.client.widget.problems.ProblemFeedbackPanel;
 import com.playshogi.website.gwt.client.widget.problems.ProblemOptionsPanelBeta;
+import org.dominokit.domino.ui.Typography.Paragraph;
+import org.dominokit.domino.ui.animations.Animation;
+import org.dominokit.domino.ui.animations.Transition;
+import org.dominokit.domino.ui.dialogs.MessageDialog;
+import org.dominokit.domino.ui.icons.Icon;
+import org.dominokit.domino.ui.icons.Icons;
+import org.dominokit.domino.ui.notifications.Notification;
+import org.dominokit.domino.ui.style.Color;
+import org.dominokit.domino.ui.style.Styles;
+import org.dominokit.domino.ui.utils.DominoWindow;
 
 @Singleton
 public class TsumeView extends Composite {
@@ -32,6 +47,46 @@ public class TsumeView extends Composite {
         problemFeedbackPanel = new ProblemFeedbackPanel(gameNavigator, true);
 
         shogiBoard.setUpperRightPanel(problemFeedbackPanel);
+
+        Button whatIsTsumeshogi = new Button("What is tsumeshogi?", (ClickHandler) clickEvent -> {
+            Icon help =
+                    Icons.ALL.help().style().add(Styles.font_72, Styles.m_b_15, Color.GREEN.getStyle()).get();
+            MessageDialog customHeaderContent =
+                    MessageDialog.createMessage(
+                            "What is tsumeshogi?",
+                            "",
+                            () -> Notification.create("Dialog closed").show())
+                            .appendChild(Paragraph.create("Tsumeshogi are checkmate problems, where you have to " +
+                                    "capture the king.").bold())
+                            .appendChild(Paragraph.create("Additionally, you have to follow the rules:").alignLeft())
+                            .appendChild(Paragraph.create(" * each move needs to be a check (threatening to " +
+                                    "capture the king),").alignLeft())
+                            .appendChild(Paragraph.create(" * the king side escapes most efficiently, making the " +
+                                    "longest possible line, while").alignLeft())
+                            .appendChild(Paragraph.create(" * forcing the attacking side to use the most (ideally" +
+                                    " all) pieces in hand.").alignLeft())
+                            .appendChild(Paragraph.create(" ").alignLeft())
+                            .appendChild(Paragraph.create("Examples:").alignLeft().bold())
+                            .appendChild(Paragraph.create(" * a tsume in 1 move is one attacking move leading to " +
+                                    "mate.").alignLeft())
+                            .appendChild(Paragraph.create(" * a tsume in 3 moves is a check, a defensive move, " +
+                                    "finally checkmate.").alignLeft())
+                            .appendChild(Paragraph.create("This pattern follows the same way for longer moves. " +
+                                    "Tsumeshogi always have an odd number of moves.").alignLeft())
+                            .addOpenListener(
+                                    () ->
+                                            Animation.create(help)
+                                                    .duration(400)
+                                                    .repeat(2)
+                                                    .transition(Transition.PULSE)
+                                                    .animate())
+                            .appendHeaderChild(help);
+            customHeaderContent.open();
+        });
+        FlowPanel lowerLeftPanel = new FlowPanel();
+        lowerLeftPanel.add(whatIsTsumeshogi);
+
+        shogiBoard.setLowerLeftPanel(lowerLeftPanel);
 
         shogiBoard.getBoardConfiguration().setPlayWhiteMoves(false);
 
